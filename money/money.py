6fd511cf2838eadbc2a0ecd24dc220b3e20557e4
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-07-15 22:06:03 -0500

    Finish Chapter 8

--- money/money.py
+++ money/money.py
@@ -1,4 +1,5 @@
 
+
 class Money(object):
 
     def __init__(self, amount):
@@ -8,3 +9,32 @@ class Money(object):
         return (
             self.amount == other.amount
             and self.__class__ == other.__class__)
+
+    def times(self, multiplier):
+        raise NotImplementedError('Need to implement in concrete class.')
+
+    @staticmethod
+    def dollar(amount):
+        return Dollar(amount)
+
+    @staticmethod
+    def franc(amount):
+        return Franc(amount)
+
+
+class Dollar(Money):
+
+    def __init__(self, amount):
+        super(Dollar, self).__init__(amount)
+
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
+
+
+class Franc(Money):
+
+    def __init__(self, amount):
+        super(Franc, self).__init__(amount)
+
+    def times(self, multiplier):
+        return Franc(self.amount * multiplier)
