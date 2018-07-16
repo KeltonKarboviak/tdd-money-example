5e309ba60f6b8d90744246459f4a6a5b0be40466
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-07-15 22:14:33 -0500

    Add type annotations to money.py

--- money/money.py
+++ money/money.py
@@ -2,39 +2,39 @@
 
 class Money(object):
 
-    def __init__(self, amount):
+    def __init__(self, amount: int):
         self.amount = amount
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Money') -> bool:
         return (
             self.amount == other.amount
             and self.__class__ == other.__class__)
 
-    def times(self, multiplier):
+    def times(self, multiplier: int) -> int:
         raise NotImplementedError('Need to implement in concrete class.')
 
     @staticmethod
-    def dollar(amount):
+    def dollar(amount: int) -> 'Dollar':
         return Dollar(amount)
 
     @staticmethod
-    def franc(amount):
+    def franc(amount: int) -> 'Franc':
         return Franc(amount)
 
 
 class Dollar(Money):
 
-    def __init__(self, amount):
+    def __init__(self, amount: int):
         super(Dollar, self).__init__(amount)
 
-    def times(self, multiplier):
+    def times(self, multiplier: int) -> int:
         return Dollar(self.amount * multiplier)
 
 
 class Franc(Money):
 
-    def __init__(self, amount):
+    def __init__(self, amount: int):
         super(Franc, self).__init__(amount)
 
-    def times(self, multiplier):
+    def times(self, multiplier: int) -> int:
         return Franc(self.amount * multiplier)
