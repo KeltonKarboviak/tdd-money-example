1435236bfe4473b46033e7a13bf25bbad2432de3
Kelton Karboviak <kelton.karboviak@gmail.com>
2018-07-16 20:44:39 -0500

    Fix test_equality

--- tests/money_test.py
+++ tests/money_test.py
@@ -17,7 +17,7 @@ class MoneyTest(TestCase):
         # assert
         self.assertTrue(Money.dollar(5) == Money.dollar(5))
         self.assertFalse(Money.dollar(5) == Money.dollar(6))
-        self.assertTrue(Money.dollar(5) == Money.franc(5))
+        self.assertFalse(Money.dollar(5) == Money.franc(5))
 
     def test_currency(self):
         self.assertEqual('USD', Money.dollar(1).currency)
